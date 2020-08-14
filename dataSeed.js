cd52fde1fc947ee0417c0bc50110881fddc2ad6e
angellee177 <angelriapurnamasari17@gmail.com>
2020-08-14 18:20:37 +0700

    seeder done

--- dataSeed.js
+++ dataSeed.js
@@ -4,13 +4,76 @@ const faker   = require('faker')
 
 async function seedMentors() {
     try {
-        const mentorData = new Mentor({
-            nik       : "21720421",
-            name      : "Ani",
-            education : "Bachelor Degree",
-            occupation: "HighSchool Teacher",
-            category  : "Biology"
-        })
-        await Mentor.
+            let mentorExist  = await Mentor.findOne({ nik: '21720421'});
+            let mentorExist1 = await Mentor.findOne({ nik: '21720422'});
+
+            if(!mentorExist && !mentorExist1 ) {
+                const mentorData = new Mentor({
+                    nik       : "21720421",
+                    name      : "Ani",
+                    education : "Bachelor Degree",
+                    occupation: "HighSchool Teacher",
+                    category  : "Biology"
+                })
+                await Mentor.save(mentorData)
+        
+                const mentorData1 = new Mentor({
+                    nik       : "21720422",
+                    name      : "Budi",
+                    education : "Master Degree",
+                    occupation: "Software Developer",
+                    category  : "Android Dev"
+                })
+                await Mentor.save(mentorData1)
+
+                console.log("2 new Mentors created.");
+            }
+    }catch (err) {
+        console.log(err.message)
+    }
+}
+
+async function seedAddress() {
+    try {
+            let mentor        = await Mentor.findOne({name : 'ani'});
+            let mentor2       = await Mentor.findOne({name : 'budi'});
+            let addressExist  = await Mentor.findOne({name : 'ani', address: { $gt: [] } });
+            let addressExist2 = await Mentor.findOne({name : 'budi', address: { $gt: [] } });
+
+            if( mentor && !addressExist) {
+                const address     = faker.address.streetAddress();
+                const addressData = {
+                    mentor_id : mentor._id,
+                    address
+                }
+                let location = new Address(addressData);
+                await location.save();
+
+                Mentor.updateOne(
+                    { _id: mentor._id},
+                    { $push: { address: location._id} }
+                ).exec()
+            }
+
+            console.log("Address have been add for Ani");
+
+            if( mentor2 && !addressExist2) {
+                const address     = faker.address.streetAddress();
+                const addressData = {
+                    mentor_id : mentor2._id,
+                    address
+                }
+                let location = new Address(addressData);
+                await location.save();
+
+                Mentor.updateOne(
+                    { _id: mentor2._id},
+                    { $push: { address: location._id} }
+                ).exec()
+            }
+            console.log("Address have been add for Budi");
+            
+    } catch (err) {
+        console.log(err.message);
     }
 }
